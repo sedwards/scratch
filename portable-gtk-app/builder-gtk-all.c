e6899bd0a400463ad5bffe44b5186ab0083dd772
Steven Edwards <winehacker@gmail.com>
2023-08-04 20:48:10 -0700

    Update gtk-builder generic app to reduce diff

--- portable-gtk-app/builder-gtk-all.c
+++ portable-gtk-app/builder-gtk-all.c
@@ -53,6 +53,74 @@ about_activate (GtkAction *action)
   gtk_dialog_run (GTK_DIALOG (about_dlg));
   gtk_widget_hide (about_dlg);
 }
+
+#else
+
+static void
+remove_timeout (gpointer data)
+{
+  guint id = GPOINTER_TO_UINT (data);
+
+  g_source_remove (id);
+}
+
+static gboolean
+pop_status (gpointer data)
+{
+  gtk_statusbar_pop (GTK_STATUSBAR (data), 0);
+  g_object_set_data (G_OBJECT (data), "timeout", NULL);
+  return G_SOURCE_REMOVE;
+}
+
+static void
+status_message (GtkStatusbar *status,
+                const char   *text)
+{
+  guint id;
+
+  gtk_statusbar_push (GTK_STATUSBAR (status), 0, text);
+  id = g_timeout_add (5000, pop_status, status);
+
+  g_object_set_data_full (G_OBJECT (status), "timeout", GUINT_TO_POINTER (id), remove_timeout);
+}
+
+static void
+help_activate (GSimpleAction *action,
+               GVariant      *parameter,
+               gpointer       user_data)
+{
+  GtkWidget *status;
+
+  status = GTK_WIDGET (g_object_get_data (G_OBJECT (user_data), "status"));
+  status_message (GTK_STATUSBAR (status), "Help not available");
+ }
+
+static void
+not_implemented (GSimpleAction *action,
+                 GVariant      *parameter,
+                 gpointer       user_data)
+{
+  GtkWidget *status;
+  char *text;
+
+  text = g_strdup_printf ("Action “%s” not implemented", g_action_get_name (G_ACTION (action)));
+  status = GTK_WIDGET (g_object_get_data (G_OBJECT (user_data), "status"));
+  status_message (GTK_STATUSBAR (status), text);
+  g_free (text);
+}
+
+static GActionEntry win_entries[] = {
+  { "new", not_implemented, NULL, NULL, NULL },
+  { "open", not_implemented, NULL, NULL, NULL },
+  { "save", not_implemented, NULL, NULL, NULL },
+  { "save-as", not_implemented, NULL, NULL, NULL },
+  { "copy", not_implemented, NULL, NULL, NULL },
+  { "cut", not_implemented, NULL, NULL, NULL },
+  { "paste", not_implemented, NULL, NULL, NULL },
+  { "quit", quit_activate, NULL, NULL, NULL },
+  { "about", about_activate, NULL, NULL, NULL },
+  { "help", help_activate, NULL, NULL, NULL }
+};
 #endif
 
 GtkWidget *
@@ -64,14 +132,14 @@ do_builder (GtkWidget *do_widget)
   
   if (!window)
     {
+      GtkWidget *about;
+      GtkWidget *status;
 
       builder = gtk_builder_new ();
-      //filename = demo_find_file ("builder-gtk2.ui", NULL);
-      //gtk_builder_add_from_file (builder, filename, &err);
-#if GTK_MAJOR_VERSION < 4
-      gtk_builder_add_from_file (builder, "builder-gtk3.ui", &err);
-#else
+#if GTK_MAJOR_VERSION == 4
       gtk_builder_add_from_file (builder, "builder-gtk4.ui", &err);
+#else
+      gtk_builder_add_from_file (builder, "builder-gtk-all.ui", &err);
 #endif
       if (err)
 	{
@@ -79,19 +147,76 @@ do_builder (GtkWidget *do_widget)
 	  return NULL;
 	}
 
- #if GTK_MAJOR_VERSION < 4
+#if GTK_MAJOR_VERSION < 4
       gtk_builder_connect_signals (builder, NULL);
- #endif
+#endif
       window = GTK_WIDGET (gtk_builder_get_object (builder, "window1"));
- #if GTK_MAJOR_VERSION < 4
+#if GTK_MAJOR_VERSION < 4
       gtk_window_set_screen (GTK_WINDOW (window),
 			     gtk_widget_get_screen (do_widget));
       g_signal_connect (window, "destroy",
 			G_CALLBACK (gtk_widget_destroyed), &window);
- #endif
+#endif
     }
 
- #if GTK_MAJOR_VERSION < 4
+#if GTK_MAJOR_VERSION == 4
+      gtk_window_set_display (GTK_WINDOW (window),
+                              gtk_widget_get_display (do_widget));
+      g_object_add_weak_pointer (G_OBJECT (window), (gpointer *)&window);
+      actions = (GActionGroup*)g_simple_action_group_new ();
+      g_action_map_add_action_entries (G_ACTION_MAP (actions),
+                                       win_entries, G_N_ELEMENTS (win_entries),
+                                       window);
+      gtk_widget_insert_action_group (window, "win", actions);
+
+      controller = gtk_shortcut_controller_new ();
+      gtk_shortcut_controller_set_scope (GTK_SHORTCUT_CONTROLLER (controller),
+                                         GTK_SHORTCUT_SCOPE_GLOBAL);
+      gtk_widget_add_controller (window, controller);
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_n, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.new")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_o, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.open")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_s, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.save")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_s, GDK_CONTROL_MASK|GDK_SHIFT_MASK),
+                             gtk_named_action_new ("win.save-as")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_q, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.quit")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_c, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.copy")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_x, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.cut")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_v, GDK_CONTROL_MASK),
+                             gtk_named_action_new ("win.paste")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_F1, 0),
+                             gtk_named_action_new ("win.help")));
+      gtk_shortcut_controller_add_shortcut (GTK_SHORTCUT_CONTROLLER (controller),
+           gtk_shortcut_new (gtk_keyval_trigger_new (GDK_KEY_F7, 0),
+                             gtk_named_action_new ("win.about")));
+
+      about = GTK_WIDGET (gtk_builder_get_object (builder, "aboutdialog1"));
+      gtk_window_set_transient_for (GTK_WINDOW (about), GTK_WINDOW (window));
+      gtk_window_set_hide_on_close (GTK_WINDOW (about), TRUE);
+      g_object_set_data_full (G_OBJECT (window), "about",
+                              about, (GDestroyNotify)gtk_window_destroy);
+
+      status = GTK_WIDGET (gtk_builder_get_object (builder, "statusbar1"));
+      g_object_set_data (G_OBJECT (window), "status", status);
+
+      g_object_unref (builder);
+#endif
+
+#if GTK_MAJOR_VERSION < 4
   if (!gtk_widget_get_visible (window))
     {
       gtk_widget_show_all (window);
@@ -101,7 +226,7 @@ do_builder (GtkWidget *do_widget)
       gtk_widget_destroy (window);
       window = NULL;
     }
- #endif
+#endif
 
   return window;
 }
@@ -114,7 +239,7 @@ int main(int argc, char *argv[]) {
   do_builder(do_widget);
   //g_signal_connect (main_app_handle, "activate", G_CALLBACK (app_activate), NULL);
 #else
-  main_app_handle = gtk_application_new ("com.github.ToshioCP.lb1", G_APPLICATION_FLAGS_NONE);
+  main_app_handle = gtk_application_new ("com.github.sedwards.gtk-builder-demo", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (main_app_handle, "activate", G_CALLBACK (app_activate), NULL);
   stat = g_application_run (G_APPLICATION (main_app_handle), argc, argv);
 #endif
